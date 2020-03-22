94e58117be9914668dc0b872e136948b30f4d6ad
annakaa <akruspe@gmail.com>
2020-03-22 12:18:38 +0100

    Update getSimilarWord.py

--- getSimilarWord.py
+++ getSimilarWord.py
@@ -1,6 +1,19 @@
 import os
 import sys
+#import sqlite3
 from gensim.models.keyedvectors import KeyedVectors
+import spacy
+import numpy as np
+
+#def loadDB():
+#  connection = sqlite3.connect("German_English_dict_or.db")
+#  cursor = connection.cursor()
+
+def load_spacy_model():
+  #for similarity
+#  nlp = spacy.load('de_core_news_md')
+  nlp = spacy.load('de_core_news_sm')
+  return nlp
 
 def load_word_vectors():
   word_vectors_model_filename = "german.model"
@@ -10,27 +23,48 @@ def load_word_vectors():
   print("Be patient! This might take a while...")
   word_vectors_model = None
   try:
-    print("Attempting to load as vector-format...")
-    word_vectors_model = KeyedVectors.load_word2vec_format(word_vectors_model_filename, binary=False)
-    print("Success!")
+    print("Attempting to load as binary-format...")
+    word_vectors_model = KeyedVectors.load_word2vec_format(word_vectors_model_filename, binary=True)
   except:
     print("Failed!")
-    try:
-      print("Attempting to load as binary-format...")
-      word_vectors_model = KeyedVectors.load_word2vec_format(word_vectors_model_filename, binary=True)
-    except:
-      print("Failed!")
-      exit(0)
+    exit(0)
    
   print("Success!")
   return word_vectors_model
 
+def sort_by_lemma(similar_list, spacy_model):
+  words = ' '.join([s[0] for s in similar_list])
+  tok = spacy_model.tokenizer(words)
+  lemmas = [t.lemma_ for t in tok]
+  
+  sorted_by_lemmas = {}
+  for ii in range(len(similar_list)):
+    if lemmas[ii] not in sorted_by_lemmas:
+      sorted_by_lemmas[lemmas[ii]] = []
+    sorted_by_lemmas[lemmas[ii]].append(similar_list[ii])
+  return sorted_by_lemmas
+ 
+def most_similar_spacy(word, spacy_model, topn=15):
+  tvec = spacy_model(word).vector
+  result = spacy_model.vocab.vectors.most_similar(tvec.reshape(1,tvec.shape[0]), n=topn)
+  print(result)
+  #dec = spacy_model.vocab.vectors.find(rows=result[0][0].tolist())
+  dec_ = [spacy_model.vocab.strings[r] for r in result[0][0].tolist()]
+  #print(dec)
+  print("dec "+str(dec_))
+  return result
+
 def main(inword):
   model = load_word_vectors()
-  most_similar_topten = model.most_similar(positive=[inword], topn=10)
-  print("Most similar top-10:")
-  for i, most_similar in enumerate(most_similar_topten):
-    print(str(i + 1) + ":", most_similar)
+  spacy_model = load_spacy_model()
+  most_similar_topten = model.most_similar(positive=[inword], topn=15)
+  #most_similar_topten = most_similar_spacy(inword, spacy_model, topn=15)
+  sorted_by_lemma = sort_by_lemma(most_similar_topten, spacy_model)
+  for l in sorted_by_lemma:
+    print(l+'\n=========')
+    for w in sorted_by_lemma[l]:
+      print(w)
+    print('\n')
   
   
 if __name__=="__main__":
